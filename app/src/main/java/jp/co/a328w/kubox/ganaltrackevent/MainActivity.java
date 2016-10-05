8375e7a8c748f72711c8defda8d95fdbeb5c2d64
久保一洋 <kubox@kubox.local>
2016-10-05 17:22:22 +0900

    イベント送信実装

--- app/src/main/java/jp/co/a328w/kubox/ganaltrackevent/MainActivity.java
+++ app/src/main/java/jp/co/a328w/kubox/ganaltrackevent/MainActivity.java
@@ -2,12 +2,68 @@ package jp.co.a328w.kubox.ganaltrackevent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.analytics.HitBuilders;
+import com.google.android.gms.analytics.Tracker;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.button1) Button button1;
+    @BindView(R.id.button2) Button button2;
+    @BindView(R.id.button3) Button button3;
+
+    private Tracker mTracker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+        // Obtain the shared Tracker instance.
+        AnalyticsApplication application = (AnalyticsApplication) getApplication();
+        mTracker = application.getDefaultTracker();
+
+        mTracker.setScreenName("MainActivity");
+        mTracker.send(new HitBuilders.ScreenViewBuilder().build());
+
     }
+
+    @OnClick(R.id.button1)
+    public void onClickButton1() {
+        Log.d("test", "button1");
+        sendEvent("button", "button1");
+    }
+
+    @OnClick(R.id.button2)
+    public void onClickButton2(){
+        Log.d("test", "button2");
+        sendEvent("button", "button2");
+    }
+
+    @OnClick(R.id.button3)
+    public void onClickButton3(){
+        Log.d("test", "button3");
+        sendEvent("button", "button3");
+    }
+
+
+    private void sendEvent(String categoryName, String actionName){
+        mTracker.send(new HitBuilders.EventBuilder()
+            .setCategory(categoryName)
+            .setAction(actionName)
+            .build());
+    }
+
+
+
 }
